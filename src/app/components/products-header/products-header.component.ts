7cdb49966a466b8b6974b1e29d463d3898d890a0
eilleen maria <egutierrezacosta85@gmail.com>
2023-08-07 18:42:57 -0500

    show carts functionality

--- src/app/components/products-header/products-header.component.ts
+++ src/app/components/products-header/products-header.component.ts
@@ -8,15 +8,19 @@ import { Component, EventEmitter, OnInit, Output} from '@angular/core';
 })
 export class ProductsHeaderComponent implements OnInit{
   @Output() columnsCountChange = new EventEmitter<number>();
+  @Output() itemsCountChange = new EventEmitter<number>();
+  @Output() sortChange = new EventEmitter<string>();
   sort = 'desc';
   itemsShowCount = 12;
   constructor(){}
   ngOnInit():void{}
   onSortUpdated(newSort:string):void{
     this.sort = newSort;
+    this.sortChange.emit(newSort);
   }
   onItemsUpdated(count:number):void{
     this.itemsShowCount= count;
+    this.itemsCountChange.emit(count);
   }
   onColumnsUpdated(columns: number): void{
     this.columnsCountChange.emit(columns);
